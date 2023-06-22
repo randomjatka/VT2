5090912963a00fc6727453040ebddfa8ba33d670
Janne Ukkonen <jataukko@jalava.cc.jyu.fi>
2023-06-22 10:22:49 +0300

    Tulostaulukon ja rastilistan pohjat

--- vt2.js
+++ vt2.js
@@ -20,6 +20,9 @@
 		console.log(xmldata);
 		console.log(xmldata.documentElement);
 		console.log(xmldata.documentElement.getElementsByTagName("joukkue"));
+
+		tulokset();
+		rastit();
 	  }
 	);
 
@@ -28,4 +31,47 @@
  // ...
  // ...
  // ...
+
+function tulokset() {
+	let kaikkiJoukkueet = xmldata.documentElement.getElementsByTagName("joukkue");
+	let kohdeTaulukko = document.getElementById("tulosTaulukko");
+	//let viiteSarjat = xmldata.documentElement.getElementsByTagName("sarjat")[0]; tämä sanoo että viiteSarjat ei ole iterable, ehkä etsintälause ottaa
+	//väärän listan?
+	
+	for (let joukkue of kaikkiJoukkueet) {
+		let tr = document.createElement("tr");
+		let td = document.createElement("td");
+		let tdKaksi = document.createElement("td");
+		kohdeTaulukko.appendChild(tr);
+		tr.appendChild(td);
+		tr.appendChild(tdKaksi);
+		let sarjanID = joukkue.getAttribute("sarja");
+		//let sarjanKesto = 0;
+		/*for (let sarja of viiteSarjat) {
+			if (sarjanID == sarja.getAttribute("sarjaid")) {
+				sarjanKesto = sarja.getAttribute("kesto");
+			}
+		}
+		*/
+		//let sarjanNimi = document.querySelector("data>sarjat>sarja[sarjaid='2737134']");
+		//let sarjanKesto = sarjanNimi.kesto;
+		td.textContent = sarjanID;
+		tdKaksi.textContent = joukkue.getElementsByTagName("nimi")[0].textContent;
+		//thKaksi.textContent = joukkue.lastChild.textContent;
+	}
+}
+
+function rastit(){
+	let kaikkiRastit = xmldata.documentElement.getElementsByTagName("rasti");
+	let kohdeLista = document.getElementById("rastiLista");
+	for (let rasti of kaikkiRastit) {
+		let li = document.createElement("li");
+		kohdeLista.appendChild(li);
+		li.textContent = rasti.getAttribute("koodi");
+		//let li = document.createElement(li);
+		//let liKaksi = document.createElement(li);
+
+	}
+}
+
 }
